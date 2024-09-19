d47471cc84ec3843c9ecb4218d5b2b2ed184b7db
Tickie <veranmaina685@gmail.com>
2024-09-19 22:14:35 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,124 @@
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                    𝗫𝗟𝗜𝗖𝗢𝗡-𝗩𝟰-𝗠𝗗  𝐁𝐎𝐓                                                //
+//                                                                                                      //
+//                                         Ｖ：4.0                                                       //
+//                                                                                                      //
+//                                                                                                      //      
+//               ██╗  ██╗██╗     ██╗ ██████╗ ██████╗ ███╗   ██╗      ██╗   ██╗██╗  ██╗                  //              
+//                ██╗██╔╝██║     ██║██╔════╝██╔═══██╗████╗  ██║      ██║   ██║██║  ██║                  //
+//                ╚███╔╝ ██║     ██║██║     ██║   ██║██╔██╗ ██║█████╗██║   ██║███████║                  // 
+//                ██╔██╗ ██║     ██║██║     ██║   ██║██║╚██╗██║╚════╝╚██╗ ██╔╝╚════██║                  // 
+//               ██╔╝ ██╗███████╗██║╚██████╗╚██████╔╝██║ ╚████║       ╚████╔╝      ██║                  //
+//                ═╝  ╚═╝╚══════╝╚═╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝        ╚═══╝       ╚═╝                  // 
+//                                                                                                      //
+//                                                                                                      //
+//                                                                                                      //
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : XLICON-V4-MD
+//  * @author : salmanytofficial
+//  * @youtube : https://www.youtube.com/@s4salmanyt
+//  * @description : XLICON-V4 ,A Multi-functional whatsapp user bot.
+//*
+//*
+//base by DGXeon
+//re-upload? recode? copy code? give credit ya :)
+//Instagram: ahmmikun
+//Telegram: t.me/ahmmitech
+//GitHub: @salmanytofficial
+//WhatsApp: +923184070915
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+//   * Created By Github: DGXeon.
+//   * Credit To Xeon
+//   * © 2024 XLICON-V3-MD.
+// ⛥┌┤
+// */
+
+const fs = require('fs');
+const chalk = require('chalk');
+
+//owmner v card
+global.ytname = "V.E.R.A.N" //ur yt chanel name
+global.socialm = "GitHub: Veran350" //ur github or insta name
+global.location = "lagos, abuja, ogun state" //ur location
+
+//new
+global.botname = 'QUEEN_TENI_CLAIRE-MD' //ur bot name
+global.ownernumber = ['2347082664317'] //ur owner number, dont add more than one
+global.ownername = 'VERAN' //ur owner name
+global.websitex = "https://github.com/Veran350"
+global.wagc = "https://whatsapp.com/channel/0029VajIDgjDZ4LVnxfB1Z2s"
+global.themeemoji = '⛩'
+global.wm = "Veran Bot Inc."
+global.botscript = 'https://github.com/Veran350' //script link
+global.packname = "QUEEN_TENI_CLAIRE"
+global.author = "MΛDΣ BY VERAN"
+global.creator = "https://wa.me/+2347082664317"
+global.xprefix = '.'
+global.premium = ["7082664317"] // Premium User
+
+//bot sett
+global.typemenu = 'v12' // menu type 'v1' => 'v12'
+global.typereply = 'v4' // reply type 'v1' => 'v4'
+global.autoblocknumber = '212' //set autoblock country code
+global.antiforeignnumber = '91' //set anti foreign number country code
+global.antidelete = false //set anti delete 
+
+
+global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
+global.tempatDB = 'database.json'
+
+//---------------------------------------------------------------------------------------//
+//api variables 
+
+global.api = "https://api.maher-zubair.xyz/";
+global.id = "RnJlZV9rZXlAc2FsbWFu";
+
+//---------------------------------------------------------------------------------------//
+
+
+
+// Description: This file is used to store global variables.
+
+
+global.limit = {
+	free: 100,
+	premium: 999,
+	vip: 'VIP'
+}
+
+global.uang = {
+	free: 10000,
+	premium: 1000000,
+	vip: 10000000
+}
+
+global.mess = {
+	error: 'Error!',
+	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+	done: 'Done'
+}
+
+global.bot = {
+	limit: 0,
+	uang: 0
+}
+
+global.game = {
+	suit: {},
+	menfes: {},
+	tictactoe: {},
+	kuismath: {},
+	tebakbom: {},
+}
+
+//~~~~~~~~~~~~~~~< PROCESS >~~~~~~~~~~~~~~~\\
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+});
